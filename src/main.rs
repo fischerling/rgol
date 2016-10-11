0daed524d60703c5dcc4e74b26e614118b514569
Florian Fischer <florian.fl.fischer@fau.de>
2016-10-11 13:39:06 +0200

    mention license in every file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+/*
+* Copyright (c) 2016, Florian Fischer
+*
+* This file is licensed under the conditions found in the LICENSE file in the
+* projects root.
+*/
+
 extern crate termion;
 
 use termion::input::TermRead;
